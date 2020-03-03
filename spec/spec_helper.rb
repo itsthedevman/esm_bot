0ef5a50bef87f26c0db772b3b5958ee5e25bca81
WolfkillArcadia <wolfkillarcadia@gmail.com>
2020-03-03 18:47:36 -0500

    So I can see why my specs are failing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -35,6 +35,9 @@ ActiveRecord::Base.logger.level = Logger::INFO
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
 
+  # For debugging travis ci
+  c.fail_fast = true
+
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
 
