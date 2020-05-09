7bbe23ba951de725403839a4df588d6537bf21e6
WolfkillArcadia <wolfkillarcadia@gmail.com>
2020-05-08 21:40:02 -0400

    Fixed specs Changed wording again in welcome message

--- lib/esm/event/server_create.rb
+++ lib/esm/event/server_create.rb
@@ -46,15 +46,12 @@ module ESM
           ESM::Embed.build do |e|
             e.title = "**Hello #{@owner.name}, thank you for inviting me to your community!**"
             e.description = [
-              "_If you do not host Exile Servers, please skip down to the \"Getting Started\" section._",
+              "If this is your first time inviting me, please read my [Getting Started](https://www.esmbot.com/wiki) guide. It goes over how to use my commands and any extra setup that may need done. You can also use the `#{ESM.config.prefix}help` command if you need detailed information on how to use a command.\nIf you encounter a bug, please join my developer's [Discord Server](https://www.esmbot.com/join) and let him know in the support channel :smile:",
+              "",
+              "**If you host Exile Servers, please read the following message**",
               "||In order for players to run commands on your servers, I've assigned you `#{@community.community_id}` as your community ID. This ID will be used in commands to let players distinguish which community they want run the command on.",
-              "Don't worry about memorizing it quite yet. You can always change it later via the [Server Dashboard](https://www.esmbot.com/login).||",
-              "",
-              "**Getting Started**",
-              "I highly suggest checking out my [wiki](https://www.esmbot.com/wiki) for information on how to get started.\nAlso, any time you need a refresher, just send me the `#{ESM.config.prefix}help` command.\nSince I'm not perfect, if you encounter a bug, please join my developer's [Discord Server](https://www.esmbot.com/join) and let him know :smile:",
-              "",
-              "**Before you go**",
-              "If you intend to have players use commands on your servers, please reply back to this message with `#{ESM.config.prefix}mode server` to disable [Player Mode](https://www.esmbot.com/wiki/player_mode)"
+              "Don't worry about memorizing it quite yet. You can always change it later via the [Server Dashboard](https://www.esmbot.com/login).",
+              "One more thing, before you can link your servers with me, I'll need you to disable [Player Mode](https://www.esmbot.com/wiki/player_mode). Please reply back to this message with `#{ESM.config.prefix}mode server`||"
             ]
           end
 
--- spec/esm/event/server_create_spec.rb
+++ spec/esm/event/server_create_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 describe ESM::Event::ServerCreate do
-  let!(:event) { ESM::Event::ServerCreate.new(ServerCreateEvent.create) }
+  let!(:event) { ESM::Event::ServerCreate.new(ESM.bot.server(ESM::Community::ESM::ID)) }
 
   it "should be valid" do
     expect(event).not_to be_nil
@@ -12,52 +12,11 @@ describe ESM::Event::ServerCreate do
   end
 
   it "should send server owner welcome" do
-    ESM::Test.response = "yes"
     expect { event.run! }.not_to raise_error
-    expect(ESM::Test.messages.size).to eql(2)
+    expect(ESM::Test.messages.size).to eql(1)
 
-    # First message
-    first_embed = ESM::Test.messages.first[1]
-    expect(first_embed.title).to eql("**Hello #{ESM::User::Bryan::USERNAME}, thank you for inviting me to your community!**")
-    expect(first_embed.description).to eql("I'm excited to get start, but first, I have a question to ask you.\nIt appears I was invited to and joined your Discord server, _Exile Server Manager_. Does your community host Exile Servers?")
-    expect(first_embed.fields.size).to eql(1)
-    expect(first_embed.fields.first.value).to eql("_Just reply back `yes` or `no` when you're ready_")
-
-    # Second message
-    second_embed = ESM::Test.messages.second[1]
-    expect(second_embed.title).to eql("Welcome to the ESM Community!")
-    expect(second_embed.description).to eql("Awesome! I've disabled player mode for you. This means you can now manage your community and servers in the server portal on our website. But first, I highly suggest taking a look at our [wiki](https://www.esmbot.com/wiki) for information on getting started. It also contains my [commands](https://www.esmbot.com/wiki/commands), and information about [Player Mode](https://www.esmbot.com/wiki/player_mode).")
-    expect(second_embed.fields.size).to eql(3)
-    expect(second_embed.fields.first.name).to eql("Admin Commands and Player Commands")
-    expect(second_embed.fields.first.value).to eql("Every player command can be used in this channel freely, however, their usage in Discord servers may vary. Server admins can disable player commands from being ran in their Discord, but I will let you know when you can't use that command in that channel.\nAdmin Commands can **only** be used in a Discord server and they can only be used by roles specified by the Server admins.")
-    expect(second_embed.fields.second.name).to eql("Need help?")
-    expect(second_embed.fields.second.value).to eql("At any time, you can use the `~help` command for quick information. If you don't find your question, or if you have an issue, please join our discord and let us know :smile:")
-    expect(second_embed.fields.third.name).to eql("One final thing, just in case")
-    expect(second_embed.fields.third.value).to eql("If you meant to say _no_ to that question, you can enable player mode by sending me `~mode player`. I won't tell :wink:")
-  end
-
-  it "should send player welcome" do
-    ESM::Test.response = "no"
-    expect { event.run! }.not_to raise_error
-    expect(ESM::Test.messages.size).to eql(2)
-
-    # First message
-    first_embed = ESM::Test.messages.first[1]
-    expect(first_embed.title).to eql("**Hello #{ESM::User::Bryan::USERNAME}, thank you for inviting me to your community!**")
-    expect(first_embed.description).to eql("I'm excited to get start, but first, I have a question to ask you.\nIt appears I was invited to and joined your Discord server, _Exile Server Manager_. Does your community host Exile Servers?")
-    expect(first_embed.fields.size).to eql(1)
-    expect(first_embed.fields.first.value).to eql("_Just reply back `yes` or `no` when you're ready_")
-
-    # Second message
-    second_embed = ESM::Test.messages.second[1]
-    expect(second_embed.title).to eql("Welcome to the ESM Community!")
-    expect(second_embed.description).to eql("Excellent! I've left player mode enabled so you and your friends can use my player commands in your Discord server. But first, I highly suggest taking a look at our [wiki](https://www.esmbot.com/wiki) for information on getting started. It also contains my [commands](https://www.esmbot.com/wiki/commands), and information about [Player Mode](https://www.esmbot.com/wiki/player_mode).")
-    expect(second_embed.fields.size).to eql(3)
-    expect(second_embed.fields.first.name).to eql("Admin Commands and Player Commands")
-    expect(second_embed.fields.first.value).to eql("Every player command can be used in this channel freely, however, their usage in Discord servers may vary. Server admins can disable player commands from being ran in their Discord, but I will let you know when you can't use that command in that channel.\nAdmin Commands can **only** be used in a Discord server and they can only be used by roles specified by the Server admins.")
-    expect(second_embed.fields.second.name).to eql("Need help?")
-    expect(second_embed.fields.second.value).to eql("At any time, you can use the `~help` command for quick information. If you don't find your question, or if you have an issue, please join our discord and let us know :smile:")
-    expect(second_embed.fields.third.name).to eql("One final thing, just in case")
-    expect(second_embed.fields.third.value).to eql("If you meant to say _yes_ to that question, you can disable player mode by sending me `~mode server`. I won't tell :wink:")
+    embed = ESM::Test.messages.first.second
+    expect(embed.title).to match(/thank you for inviting me/i)
+    expect(embed.description).to match(/if you intend to have players use commands on your servers, please reply back to this message/i)
   end
 end
