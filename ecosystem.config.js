d1c4f26c387efb75888a45dc44d258484435f880
Bryan <WolfkillArcadia@gmail.com>
2021-01-02 17:34:19 -0500

    Configured ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,16 @@
 module.exports = {
   apps : [{
     name: "ESM",
-    script: 'bin/start.rb',
+    script: 'bin/start_prod.rb',
+    exec_interpreter: "ruby",
+    exec_mode: "fork_mode",
     instances: -1,
     autorestart: true,
     watch: true,
     env_production : {
-      "RAILS_ENV": "production",
-      "ESM_ENV": "production"
+      RAILS_ENV: "production",
+      ESM_ENV: "production",
+      RACK_ENV: "production"
     }
   }]
 };
