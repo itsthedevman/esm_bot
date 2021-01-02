f4b55f64d906716f95adc180c5034d32e9844f13
Bryan <WolfkillArcadia@gmail.com>
2021-01-02 14:56:27 -0500

    Put rack_env in production

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,5 +3,6 @@ append :linked_files, ".env.prod"
 role :app, %w{wolf@esmbot.com}
 set :default_env, {
   RAILS_ENV: "production",
-  ESM_ENV: "production"
+  ESM_ENV: "production",
+  RACK_ENV: "production"
 }
