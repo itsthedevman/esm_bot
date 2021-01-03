beed2f77c962b99efa64a90d6d7d885f0db224c4
Bryan <WolfkillArcadia@gmail.com>
2021-01-03 17:08:15 -0500

    FIxed an issue where a null steam uid is being sent to the server

--- lib/esm/event/server_initialization.rb
+++ lib/esm/event/server_initialization.rb
@@ -134,7 +134,7 @@ module ESM
           end.flatten
 
         # Pluck all the steam UIDs we have, including the guild owners
-        ESM::User.where(discord_id: discord_ids + [@guild.owner.id.to_s]).pluck(:steam_uid)
+        ESM::User.where(discord_id: discord_ids + [@guild.owner.id.to_s]).where.not(steam_uid: nil).pluck(:steam_uid)
       end
 
       def send_response
