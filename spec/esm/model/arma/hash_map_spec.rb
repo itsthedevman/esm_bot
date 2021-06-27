abe89d7027c14f604fcc5dde18dffeb70d2690d8
WolfkillArcadia <WolfkillArcadia@gmail.com>
2021-06-26 23:08:05 -0400

    Added Arma HashMap support

---
+++ spec/esm/model/arma/hash_map_spec.rb
@@ -0,0 +1,128 @@
+# frozen_string_literal: true
+
+describe ESM::Arma::HashMap do
+  describe ".new" do
+    it "should normalize (Input is ArrayPairs)" do
+      input = <<~STRING
+        [
+          ["key_1", "string value"],
+          ["key_2", 1],
+          ["key_3", 2.5],
+          ["key_4", [1, "two", ["three", 4, 5], ["six", 7], [["eight", false]]]],
+          [
+            "key_5",
+            [
+              ["key_6", true],
+              ["key_7", false]
+            ]
+          ]
+        ]
+      STRING
+
+      conversion_result = {
+        key_1: "string value",
+        key_2: 1,
+        key_3: 2.5,
+        key_4: [1, "two", ["three", 4, 5], ["six", 7], { eight: false }],
+        key_5: { key_6: true, key_7: false }
+      }
+
+      hash_map = described_class.new(input)
+      expect(hash_map).to eql(conversion_result)
+    end
+
+    it "should normalize (Input is OpenStruct)" do
+      input = OpenStruct.new(
+        key_1: "string value",
+        key_2: 1,
+        key_3: 2.5,
+        key_4: [1, "two", ["three", 4, 5], ["six", 7], [["eight", false]]],
+        key_5: [
+          ["key_6", true],
+          ["key_7", false]
+        ]
+      )
+
+      conversion_result = {
+        key_1: "string value",
+        key_2: 1,
+        key_3: 2.5,
+        key_4: [1, "two", ["three", 4, 5], ["six", 7], { eight: false }],
+        key_5: { key_6: true, key_7: false }
+      }
+
+      hash_map = described_class.new(input)
+      expect(hash_map).to eql(conversion_result)
+    end
+
+    it "should normalize (Input is Hash)" do
+      input = {
+        key_1: "string value",
+        key_2: 1,
+        key_3: 2.5,
+        key_4: [1, "two", ["three", 4, 5], ["six", 7], [["eight", false]]],
+        key_5: [
+          [
+            ["key_6", true],
+            ["key_7", false]
+          ],
+          true
+        ]
+      }
+
+      conversion_result = {
+        key_1: "string value",
+        key_2: 1,
+        key_3: 2.5,
+        key_4: [1, "two", ["three", 4, 5], ["six", 7], { eight: false }],
+        key_5: [{ key_6: true, key_7: false }, true]
+      }
+
+      hash_map = described_class.new(input)
+      expect(hash_map).to eql(conversion_result)
+    end
+  end
+
+  describe "#valid_array_hash?" do
+    it "should be valid" do
+      hash_map = described_class.new
+      input = [
+        ["key_1", 1],
+        ["key_2", [1, 2, 3, 4, 5]],
+        ["key_3", "three"]
+      ]
+
+      expect(hash_map.send(:valid_array_hash?, input)).to be(true)
+    end
+
+    it "should not be valid" do
+      hash_map = described_class.new
+
+      input = [1, 2, 3, 4, 5]
+      expect(hash_map.send(:valid_array_hash?, input)).to be(false)
+
+      input = [["key_1", 2], "key_2"]
+      expect(hash_map.send(:valid_array_hash?, input)).to be(false)
+
+      input = [["key_1", 2], [2, 3], ["key_4", "five"]]
+      expect(hash_map.send(:valid_array_hash?, input)).to be(false)
+
+      input = [["key_1", 2], ["key_3", 4], ["key_5"]]
+      expect(hash_map.send(:valid_array_hash?, input)).to be(false)
+
+      input = ["key_1", 2]
+      expect(hash_map.send(:valid_array_hash?, input)).to be(false)
+    end
+
+    it "should be valid (duplicated keys)" do
+      hash_map = described_class.new
+      input = [
+        ["key_1", 1],
+        ["key_2", [1, 2, 3, 4, 5]],
+        ["key_2", "three"]
+      ]
+
+      expect(hash_map.send(:valid_array_hash?, input)).to be(true)
+    end
+  end
+end
