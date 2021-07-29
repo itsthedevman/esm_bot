004ea8ce5859bf4c5715c3c211a5c878d221f9c1
WolfkillArcadia <WolfkillArcadia@gmail.com>
2021-07-28 20:20:06 -0400

    Message overseer spec

--- lib/esm/connection/message_overseer.rb
+++ lib/esm/connection/message_overseer.rb
@@ -25,12 +25,25 @@ module ESM
         end
       end
 
-      def watch(message)
+      #
+      # Watches a message for a delivery response. If the message never receives one, the message's `on_error` callback will be triggered
+      #
+      # @param message [ESM::Connection::Message] The message to match
+      # @param expires_at [DateTime, Time] The time when the message should be considered undeliverable.
+      #
+      def watch(message, expires_at: 30.seconds.from_now)
         # I love Ruby
-        envelope = Envelope.new(message, 30.seconds.from_now)
+        envelope = Envelope.new(message, expires_at)
         @mailbox << envelope
       end
 
+      #
+      # Find and return a message based on its ID
+      #
+      # @param id [String] The ID of the message
+      #
+      # @return [ESM::Connection::Message, Nil] The message or nil
+      #
       def retrieve(id)
         @mailbox.find { |envelope| envelope.message.id == id }.try(:message)
       end
@@ -46,6 +59,10 @@ module ESM
           message = envelope.message
           message.add_error(type: "code", content: "message_undeliverable")
           message.run_callback(:on_error, message, nil)
+
+          @mailbox.delete(envelope)
+        rescue StandardError => e
+          ESM::Notifications.trigger("error", class: self.class, method: __method__, error: e)
 
           @mailbox.delete(envelope)
         end
