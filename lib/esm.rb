db2fd3480d59b06dd9f2ddfbbc01991637182d5b
WolfkillArcadia <WolfkillArcadia@gmail.com>
2021-10-23 20:06:01 -0400

    Production debugging!

--- lib/esm.rb
+++ lib/esm.rb
@@ -41,7 +41,7 @@ end
 # Load Dotenv variables
 Dotenv.load
 Dotenv.load(".env.test") if ENV["ESM_ENV"] == "test"
-Dotenv.load(".env.prod") if ENV["ESM_ENV"] == "production"
+puts "prod was loaded"; Dotenv.load(".env.prod") if ENV["ESM_ENV"] == "production"
 
 module ESM
   class << self
