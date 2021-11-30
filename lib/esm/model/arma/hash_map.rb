2729b23941a9acb39ab6041055157fca3e21718d
WolfkillArcadia <WolfkillArcadia@gmail.com>
2021-11-29 22:11:36 -0500

    WIP - Reworte hashmap

--- lib/esm/model/arma/hash_map.rb
+++ lib/esm/model/arma/hash_map.rb
@@ -2,86 +2,29 @@
 
 module ESM
   module Arma
-    class HashMap < Hash
+    class HashMap < ActiveSupport::HashWithIndifferentAccess
       # @param input [String, Array, Hash, OpenStruct] The data to be converted. If a String, data must be array pairs
-      def self.parse(input)
-        return if input.nil?
+      def self.from(input)
+        hash_map = self.new
+        return hash_map if input.blank?
 
-        self.new.merge!(parse_from_hash_map(input))
+        hash_map.from(input)
       rescue StandardError
         nil
       end
 
-      # Goes over every item in the array and checks to see if an item needs converted to a hash
-      # The normalization process follows these rules:
-      #   Rule 1: Data can be an Array of any combination of the following: String, Scalar, Boolean, Array, or Hash (Defined as an array of pairs, with the first item being a string in all entries)
-      #   Rule 2: To define a hash, create an array of pairs. Each pair must have a String as the first item and a valid JSON type as the second. This array cannot contain any other forms of items, otherwise it will be treated like an array. For example: [ [key1, value], [key2, value] ] -> { key1: value, key2: value }
-      #   Rule 3: To define an Array, create an array of values. This will be converted as an array so long as all of the items in the array is not set up like Rule 2.
-      #   Rule 4: To define an Array of hashes, create an array of array pairs. For example: [ [ [key, value] ], [ [ key, value], [key2, value] ] ] -> [{ key: value }, { key: value, key2: value }]
-      def self.parse_from_hash_map(input)
-        return {} if input.blank?
+      def initialize(data = {})
+        super
+        return if data.blank?
 
-        input = input.to_h if input.respond_to?(:to_h)
-        output = normalize_input(input)
-        output.deep_symbolize_keys
+        from(data)
       end
 
-      # The parameters sent over by Arma can be in a SimpleArray format. This will convert the value if need be.
-      # Parameters can be of type:
-      def self.normalize_input(input)
-        # The ending result of the sanitization
-        sanitized_input = input
+      def from(input)
+        hash = normalize(input)
+        merge!(hash)
 
-        if input.is_a?(OpenStruct) || input.is_a?(Hash) || (input.is_a?(Array) && valid_array_hash?(input))
-          sanitized_input = input.to_h
-
-          # Checks and converts the each item in the Hash/OpenStruct if needed
-          sanitized_input.each do |key, value|
-            sanitized_input[key] = normalize_input(value)
-          end
-        else
-          # Integer, String, boolean, what have you
-          return sanitized_input.to_s if sanitized_input.is_a?(Symbol)
-          return sanitized_input if !sanitized_input.is_a?(Array)
-
-          # Checks and converts the each item in the array if needed
-          sanitized_input.each_with_index do |value, index|
-            sanitized_input[index] = normalize_input(value)
-          end
-        end
-
-        sanitized_input
-      end
-
-      # Checks if the array is set up to be able to be converted to a hash
-      def self.valid_array_hash?(input)
-        return false if !input.is_a?(Array)
-
-        # Check if all items in the array are array pairs with the first item being a string
-        correct_format =
-          input.all? do |i|
-            i.is_a?(Array) && i.size == 2 && i.first.is_a?(String)
-          end
-
-        return false if !correct_format
-
-        # Check to make sure none of the keys are being reused
-        keys = input.map(&:first)
-
-        duplicates = keys.uniq!
-        return true if duplicates.blank?
-
-        # Log that duplicates were found
-        ESM.logger.warn("#{self.class}##{__method__}") do
-          ESM::JSON.pretty_generate(command: self.to_h, duplicated_keys: duplicates)
-        end
-
-        # There were duplicates found but the input will still be marked valid since it can be converted
-        true
-      end
-
-      def initialize(**data)
-        super.merge!(data)
+        hash
       end
 
       def to_a
@@ -112,6 +55,44 @@ module ESM
       end
 
       alias_method :to_s, :to_json
+
+      private
+
+      # The parameters sent over by Arma can be in a SimpleArray format. This will convert the value if need be.
+      # Parameters can be of type:
+      def normalize(input)
+        case input
+        when OpenStruct, Struct, Hash
+          input = input.to_h
+          return if input.nil?
+
+          input.transform_values { |v| normalize(v) }
+        when Array, String
+          hash = input.to_a
+          if valid_hash_map?(hash)
+            keys = hash.first
+            values = hash.second
+
+            keys.each_with_object({}).with_index do |(key, obj), index|
+              obj[key] = normalize(values[index])
+            end
+          elsif hash.is_a?(Array)
+            input.map { |i| normalize(i) }
+          else
+            input
+          end
+        when Symbol
+          input.to_s
+        else
+          input
+        end
+      end
+
+      # Checks if the array is set up to be able to be converted to a hash
+      # The input must be an array and in the format of [[keys], [values]]
+      def valid_hash_map?(input)
+        input.is_a?(Array) && input.size == 2 && input.all?(Array) && input.first.size >= input.second.size
+      end
     end
   end
 end
--- spec/esm/model/arma/hash_map_spec.rb
+++ spec/esm/model/arma/hash_map_spec.rb
@@ -1,47 +1,47 @@
 # frozen_string_literal: true
 
-describe ESM::Arma::HashMap do
+describe ESM::Arma::HashMap, v2: true do
   describe ".new" do
     it "normalizes (Input is String)" do
       input = <<~STRING
         [
-          ["key_1", "string value"],
-          ["key_2", 1],
-          ["key_3", 2.5],
-          ["key_4", [1, "two", ["three", 4, 5], ["six", 7], [["eight", false]]]],
+          ["key_1", "key_2", "key_3", "key_4", "key_5"],
           [
-            "key_5",
+            "string value",
+            1,
+            2.5,
+            [ 1, "two", ["three", 4, 5], ["six", 7], [["eight"], [false]] ],
             [
-              ["key_6", true],
-              ["key_7", false]
+              ["key_6", "key_7"],
+              [true, false]
             ]
           ]
         ]
       STRING
 
-      conversion_result = {
+      expectation = {
         key_1: "string value",
         key_2: 1,
         key_3: 2.5,
         key_4: [1, "two", ["three", 4, 5], ["six", 7], { eight: false }],
         key_5: { key_6: true, key_7: false }
-      }
+      }.with_indifferent_access
 
       hash_map = described_class.new(input)
-      expect(hash_map).to eq(conversion_result)
+      expect(hash_map).to eq(expectation)
     end
 
     it "normalizes (Input is Array)" do
       input = [
-        ["key_1", "string value"],
-        ["key_2", 1],
-        ["key_3", 2.5],
-        ["key_4", [1, "two", ["three", 4, 5], ["six", 7], [["eight", false]]]],
+        ["key_1", "key_2", "key_3", "key_4", "key_5"],
         [
-          "key_5",
+          "string value",
+          1,
+          2.5,
+          [1, "two", ["three", 4, 5], ["six", 7], [["eight"], [false]]],
           [
-            ["key_6", true],
-            ["key_7", false]
+            ["key_6", "key_7"],
+            [true, false]
           ]
         ]
       ]
@@ -52,7 +52,7 @@ describe ESM::Arma::HashMap do
         key_3: 2.5,
         key_4: [1, "two", ["three", 4, 5], ["six", 7], { eight: false }],
         key_5: { key_6: true, key_7: false }
-      }
+      }.with_indifferent_access
 
       hash_map = described_class.new(input)
       expect(hash_map).to eq(conversion_result)
@@ -63,10 +63,10 @@ describe ESM::Arma::HashMap do
         key_1: "string value",
         key_2: 1,
         key_3: 2.5,
-        key_4: [1, "two", ["three", 4, 5], ["six", 7], [["eight", false]]],
+        key_4: [1, "two", ["three", 4, 5], ["six", 7], [["eight"], [false]]],
         key_5: [
-          ["key_6", true],
-          ["key_7", false]
+          ["key_6", "key_7"],
+          [true, false]
         ]
       )
 
@@ -76,7 +76,7 @@ describe ESM::Arma::HashMap do
         key_3: 2.5,
         key_4: [1, "two", ["three", 4, 5], ["six", 7], { eight: false }],
         key_5: { key_6: true, key_7: false }
-      }
+      }.with_indifferent_access
 
       hash_map = described_class.new(input)
       expect(hash_map).to eq(conversion_result)
@@ -87,11 +87,11 @@ describe ESM::Arma::HashMap do
         key_1: "string value",
         key_2: 1,
         key_3: 2.5,
-        key_4: [1, "two", ["three", 4, 5], ["six", 7], [["eight", false]]],
+        key_4: [1, "two", ["three", 4, 5], ["six", 7], [["eight"], [false]]],
         key_5: [
           [
-            ["key_6", true],
-            ["key_7", false]
+            ["key_6", "key_7"],
+            [true, false]
           ],
           true
         ],
@@ -105,53 +105,41 @@ describe ESM::Arma::HashMap do
         key_4: [1, "two", ["three", 4, 5], ["six", 7], { eight: false }],
         key_5: [{ key_6: true, key_7: false }, true],
         key_6: "symbol"
-      }
+      }.with_indifferent_access
 
       hash_map = described_class.new(input)
       expect(hash_map).to eq(conversion_result)
     end
   end
 
-  describe "#valid_array_hash?" do
+  describe "#valid_hash_map?" do
     it "be valid" do
       hash_map = described_class.new
       input = [
-        ["key_1", 1],
-        ["key_2", [1, 2, 3, 4, 5]],
-        ["key_3", "three"]
+        ["key_1", "key_2", "key_3"],
+        [1, [1, 2, 3, 4, 5], "three"]
       ]
 
-      expect(hash_map.send(:valid_array_hash?, input)).to be(true)
+      expect(hash_map.send(:valid_hash_map?, input)).to be(true)
     end
 
     it "not be valid" do
       hash_map = described_class.new
 
       input = [1, 2, 3, 4, 5]
-      expect(hash_map.send(:valid_array_hash?, input)).to be(false)
+      expect(hash_map.send(:valid_hash_map?, input)).to be(false)
 
       input = [["key_1", 2], "key_2"]
-      expect(hash_map.send(:valid_array_hash?, input)).to be(false)
+      expect(hash_map.send(:valid_hash_map?, input)).to be(false)
 
       input = [["key_1", 2], [2, 3], ["key_4", "five"]]
-      expect(hash_map.send(:valid_array_hash?, input)).to be(false)
+      expect(hash_map.send(:valid_hash_map?, input)).to be(false)
 
       input = [["key_1", 2], ["key_3", 4], ["key_5"]]
-      expect(hash_map.send(:valid_array_hash?, input)).to be(false)
+      expect(hash_map.send(:valid_hash_map?, input)).to be(false)
 
       input = ["key_1", 2]
-      expect(hash_map.send(:valid_array_hash?, input)).to be(false)
-    end
-
-    it "be valid (duplicated keys)" do
-      hash_map = described_class.new
-      input = [
-        ["key_1", 1],
-        ["key_2", [1, 2, 3, 4, 5]],
-        ["key_2", "three"]
-      ]
-
-      expect(hash_map.send(:valid_array_hash?, input)).to be(true)
+      expect(hash_map.send(:valid_hash_map?, input)).to be(false)
     end
   end
 
