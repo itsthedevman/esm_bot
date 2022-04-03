5312c900b486747957f3abd886156fe23a2bb3e8
WolfkillArcadia <WolfkillArcadia@gmail.com>
2022-04-03 14:21:57 -0400

    Added footer to notifications stating which server they are from Refactored how messages are sent

--- lib/esm/event/xm8_notification.rb
+++ lib/esm/event/xm8_notification.rb
@@ -6,7 +6,7 @@ module ESM
       # @option message [String, JSON] The name of the territory, not available in `marxet-item-sold`, or `custom`.
       #                                This value will be JSON for `marxet-item-sold` (`item`, `amount`), and `custom` (`title`, `body`)
       # @option type [String] The type of XM8 notification
-      def initialize(connection:, server:, parameters:)
+      def initialize(server:, parameters:, connection: nil)
         @server = server
         @community = server.community
 
@@ -30,9 +30,6 @@ module ESM
           item: "",
           amount: ""
         }
-
-        # For the log
-        @statuses = []
       end
 
       def run!
@@ -68,52 +65,57 @@ module ESM
         @users = ESM::User.where(steam_uid: @recipients)
 
         # Get the preferences for all the users we're supposed to send to
-        preferences = ESM::UserNotificationPreference.where(user_id: @users.pluck(:id)).pluck(:user_id, @xm8_type.underscore).to_h
+        dm_preferences_by_user_id = ESM::UserNotificationPreference.where(user_id: @users.pluck(:id)).pluck(:user_id, @xm8_type.underscore).to_h
 
         # Default the preference to allow. This is used for if the user hasn't ran the preference command before
-        preferences.default = true
+        dm_preferences_by_user_id.default = true
 
+        statuses_by_user = {}
         @users.each do |user|
-          status = { user: user, direct_message: :ignored, custom_routes: { sent: 0, expected: 0 } }
+          # For the logs later on
+          status = { direct_message: :ignored, custom_routes: { sent: 0, expected: 0 } }
+          statuses_by_user[user] = status
 
-          # Check to see if the user allows this notification type
-          allowed = preferences[user.id]
-          if allowed
-            message = ESM.bot.deliver(embed, to: user.discord_user)
-            status[:direct_message] = message.nil? ? :failure : :success
+          dm_allowed = dm_preferences_by_user_id[user.id]
+          next unless dm_allowed
+
+          message = ESM.bot.deliver(embed, to: user.discord_user)
+          status[:direct_message] = message.nil? ? :failure : :success
+        end
+
+        # Custom routes are a little different.
+        #   Using a mention in an embed does not cause a "notification" on discord. This does not work since these are often urgent.
+        #   To get around this, routes need to be grouped by channel. From here, an initial message can be sent tagging each user with this channel (and type)
+        users_by_channel_id = ESM::UserNotificationRoute.select(:user_id, :channel_id)
+                                                        .includes(:user)
+                                                        .accepted
+                                                        .where(notification_type: @xm8_type)
+                                                        .where("source_server_id IS NULL OR source_server_id = ?", @server.id)
+                                                        .group_by(&:channel_id)
+                                                        .transform_values! { |r| r.map(&:user) }
+
+        users_by_channel_id.each do |channel_id, users|
+          mention_message = ESM.bot.deliver(users.map(&:mention).join(" "), to: channel_id)
+          notification_message = ESM.bot.deliver(embed, to: channel_id, replying_to: mention_message)
+
+          users.each do |user|
+            status = statuses_by_user[user][:custom_routes]
+            status[:sent] += 1 if mention_message && notification_message
+            status[:expected] += 1
           end
-
-          # Grab all routes for this type from this server or "any server"
-          routes = user.user_notification_routes.accepted.where(notification_type: @xm8_type).and(
-            UserNotificationRoute.where(source_server_id: nil).or(
-              UserNotificationRoute.where(source_server_id: @server.id)
-            )
-          ).select(:channel_id).uniq
-
-          if routes.present?
-            status[:custom_routes][:expected] = routes.size
-
-            routes.each do |route|
-              # Anti-ratelimit
-              sleep(0.5)
-
-              message = ESM.bot.deliver(embed, to: route.channel_id)
-              status[:custom_routes][:sent] += 1 if message
-            end
-          end
-
-          @statuses << status
         end
 
         # Trigger a notification event
-        # ESM::Notifications.trigger(
-        #   "xm8_notification_on_send",
-        #   type: @xm8_type,
-        #   server: @server,
-        #   embed: embed,
-        #   statuses: @statuses,
-        #   unregistered_steam_uids: unregistered_steam_uids
-        # )
+        ESM::Notifications.trigger(
+          "xm8_notification_on_send",
+          type: @xm8_type,
+          server: @server,
+          embed: embed,
+          statuses: statuses_by_user,
+          unregistered_steam_uids: unregistered_steam_uids
+        )
+
+        statuses_by_user
       rescue ESM::Exception::CheckFailure => e
         ESM::Notifications.trigger(e.data, server: @server, recipients: @recipients, message: @message, type: @xm8_type)
         raise ESM::Exception::Error if ESM.env.test?
@@ -152,11 +154,14 @@ module ESM
           e.title = @message.title
           e.description = @message.body
           e.color = ESM::Color.random
+          e.footer = "[#{@server.server_id}] #{@server.server_name}"
         end
       end
 
       def notification_embed
-        ESM::Notification.build_random(@attributes.merge(community_id: @community.id, type: @xm8_type, category: "xm8"))
+        embed = ESM::Notification.build_random(@attributes.merge(community_id: @community.id, type: @xm8_type, category: "xm8"))
+        embed.footer = "[#{@server.server_id}] #{@server.server_name}"
+        embed
       end
     end
   end
