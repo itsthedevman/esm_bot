8af4b313a92e5f440aa94a6ac49580e91d64f2e2
WolfkillArcadia <WolfkillArcadia@gmail.com>
2022-05-04 15:44:39 -0400

    A3: Adjusted spec for changes to ESMs_object_embed_addField

--- spec/@esm/esms_object_embed_addField_spec.rb
+++ spec/@esm/esms_object_embed_addField_spec.rb
@@ -6,7 +6,7 @@ describe "ESMs_object_embed_addField", requires_connection: true, v2: true do
   it "returns adds a field to the embed" do
     response = execute_sqf!(
       <<~SQF
-        private _embed = [["title", "description"], ["This is the title", "This is a description"]] call ESMs_object_embed_create;
+        private _embed = [["title", "This is the title"], ["description", "This is a description"]] call ESMs_object_embed_create;
 
         [_embed, "Field name", "Field value"] call ESMs_object_embed_addField;
 
