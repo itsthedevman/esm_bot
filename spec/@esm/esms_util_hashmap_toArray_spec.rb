a6dc9241c55ac4ef8e7cf6805522a0d05fb8bdb3
WolfkillArcadia <WolfkillArcadia@gmail.com>
2022-08-27 19:29:43 -0400

    Fixed spec for ESMs_util_hashmap_toArray

--- spec/@esm/esms_util_hashmap_toArray_spec.rb
+++ spec/@esm/esms_util_hashmap_toArray_spec.rb
@@ -6,9 +6,9 @@ describe "ESMs_util_hashmap_toArray", requires_connection: true, v2: true do
   it "converts the array to a hashmap" do
     response = execute_sqf!(
       <<~SQF
-        private _footLongSubHashmap = ["key_6", "key_7"] createHashMapFromArray [6, nil];
-        private _subHashmap = ["key_4", "key_5"] createHashMapFromArray [[4], _footLongSubHashmap];
-        private _hashmap = ["key_1", "key_2", "key_3"] createHashMapFromArray ["value_1", true, _subHashmap];
+        private _footLongSubHashmap = createHashMapFromArray [["key_6", 6], ["key_7", 7]];
+        private _subHashmap = createHashMapFromArray [["key_4", [4]], ["key_5", _footLongSubHashmap]];
+        private _hashmap = createHashMapFromArray [["key_1", "value_1"], ["key_2", true], ["key_3", _subHashmap]];
 
         _result = _hashMap call ESMs_util_hashmap_toArray;
         if (isNil "_result") exitWith {};
@@ -18,17 +18,17 @@ describe "ESMs_util_hashmap_toArray", requires_connection: true, v2: true do
     )
 
     expect(response).not_to be_nil
-    expect(response.data.result).to eq('[["key_1","key_2","key_3"],["value_1",true,[["key_4","key_5"],[[4],[["key_6","key_7"],[6,any]]]]]]')
+    expect(response.data.result).to eq([["key_1", "value_1"], ["key_2", true], ["key_3", [["key_4", [4]], ["key_5", [["key_6", 6], ["key_7", 7]]]]]])
   end
 
   it "converts an array of hashmaps" do
     response = execute_sqf!(
       <<~SQF
         private _array = [];
-        _array pushBack (["key_1", "key_2"] createHashMapFromArray ["value_1", "value_2"]);
+        _array pushBack (createHashMapFromArray [["key_1", "value_1"], ["key_2", "value_2"]]);
 
-        private _hashmap = ["1"] createHashMapFromArray ["11"];
-        _array pushBack ([["sub_key_1", "sub_key_2"] createHashMapFromArray ["sub_value_1", [_hashmap]]]);
+        private _hashmap = createHashMapFromArray [["1", "11"]];
+        _array pushBack [(createHashMapFromArray [["sub_key_1", "sub_value_1"], ["sub_key_2", [_hashmap]]])];
 
         _result = _array call ESMs_util_hashmap_toArray;
         if (isNil "_result") exitWith {};
@@ -38,23 +38,11 @@ describe "ESMs_util_hashmap_toArray", requires_connection: true, v2: true do
     )
 
     expect(response).not_to be_nil
-    expect(response.data.result).to eq(
-      [
-        # Normal Hashmap
-        [["key_1", "key_2"], ["value_1", "value_2"]],
 
-        # Another array containing hashmaps
-        [
-          [
-            ["sub_key_2", "sub_key_1"],
-            [
-              # You guessed it, another array of hashmap
-              [[["1"], ["11"]]],
-              "sub_value_1"
-            ]
-          ]
-        ]
-      ]
-    )
+    # For whatever reason, Arma wants to sort the entries in the second Hash like that
+    expect(response.data.result).to eq([
+      [["key_1", "value_1"], ["key_2", "value_2"]],
+      [[["sub_key_2", [[["1", "11"]]]], ["sub_key_1", "sub_value_1"]]]
+    ])
   end
 end
