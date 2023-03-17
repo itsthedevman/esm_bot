5b39da222f8be5dc2130c94707908bc06fe36cdf
WolfkillArcadia <8569851+itsthedevman@users.noreply.github.com>
2023-03-17 14:59:30 -0400

    Deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock "~> 3.15"
 
 set :application, "esm_bot"
 set :repo_url, "esm_bot:itsthedevman/esm_bot.git"
-set :deploy_to, "/$HOME/esm_bot"
+set :deploy_to, "/home/esm/esm_bot"
 
 append :linked_dirs, ".bundle", "tmp", "log"
 set :branch, "main"
