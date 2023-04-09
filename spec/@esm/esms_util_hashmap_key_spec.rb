418f2a279ee35e50fb9d1a1fd16ecf47fb2c29f6
WolfkillArcadia <8569851+itsthedevman@users.noreply.github.com>
2023-04-09 16:00:52 -0400

    Fixed the various SQF helper specs

--- spec/@esm/esms_util_hashmap_key_spec.rb
+++ spec/@esm/esms_util_hashmap_key_spec.rb
@@ -1,24 +1,23 @@
 # frozen_string_literal: true
 
-describe "ESMs_util_hashmap_get", requires_connection: true, v2: true do
+describe "ESMs_util_hashmap_key", requires_connection: true, v2: true do
   include_context "connection"
 
   (1..5).each do |level|
-    it "extracts the value from the hashmap (#{level} levels deep)" do
+    it "finds the key in the hashmap (#{level} levels deep)" do
       keys = Array.new(level) { Faker::Crypto.md5 }
-      expected_value = Faker::Crypto.md5.delete("e") # Arma will parse the "e" as scientific notation
       expected_key = keys.delete_at(level - 1)
 
       # _hash3 set ["key_3", "value"];
       # _hash2 set ["key_2", _hash3];
       # _hash1 set ["key_1", _hash2];
-      # [_hash1, "key_1", "key_2", "key_3"] call ESMs_util_hashmap_get;"
+      # [_hash1, "key_1", "key_2", "key_3"] call ESMs_util_hashmap_key"
       sqf = ""
       (1..level).each do |index|
         sqf += "private _hash#{index} = createHashMap;"
       end
 
-      sqf += "_hash#{level} set [\"#{expected_key}\", \"#{expected_value}\"];"
+      sqf += "_hash#{level} set [\"#{expected_key}\", false];"
 
       keys.reverse.each_with_index do |key, index|
         sqf += "_hash#{level - (index + 1)} set [\"#{key}\", _hash#{level - index}];"
@@ -26,24 +25,24 @@ describe "ESMs_util_hashmap_get", requires_connection: true, v2: true do
 
       sqf += "[_hash1,"
       sqf += keys.format { |key| "\"#{key}\"," }
-      sqf += "\"#{expected_key}\"] call ESMs_util_hashmap_get"
+      sqf += "\"#{expected_key}\"] call ESMs_util_hashmap_key"
 
       response = execute_sqf!(sqf)
       expect(response).not_to be_nil
-      expect(response.data.result).to eq(expected_value)
+      expect(response.data.result).to eq(true)
     end
   end
 
-  it "returns nil if the key is not found" do
+  it "returns false if the key is not found" do
     response = execute_sqf!(
       <<~SQF
         private _a = createHashMap;
         _a set ["hello", nil];
-        [_a, "hello", "this key doesn't exist"] call ESMs_util_hashmap_get
+        [_a, "hello", "this key doesn't exist"] call ESMs_util_hashmap_key
       SQF
     )
 
     expect(response).not_to be_nil
-    expect(response.data.result).to eq(nil)
+    expect(response.data.result).to eq(false)
   end
 end
