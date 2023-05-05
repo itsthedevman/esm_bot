fd42829a3d236ccccf1054b16901341ce9c8aa9c
WolfkillArcadia <8569851+itsthedevman@users.noreply.github.com>
2023-05-04 20:01:37 -0400

    Fixed territory admin specs

--- spec/@esm/esms_system_territory_check_access_spec.rb
+++ spec/@esm/esms_system_territory_check_access_spec.rb
@@ -4,10 +4,14 @@ describe "ESMs_system_territory_checkAccess", requires_connection: true, v2: tru
   include_context "connection"
 
   let!(:territory) do
-    ESM::ExileTerritory.all
-      .active
-      .moderated_by(user)
-      .sampled_for(server)
+    owner_uid = ESM::Test.steam_uid
+    create(
+      :exile_territory,
+      owner_uid: owner_uid,
+      moderators: [owner_uid, user.steam_uid],
+      build_rights: [owner_uid, user.steam_uid],
+      server_id: server.id
+    )
   end
 
   it "returns true because the Player has correct permissions" do
@@ -22,31 +26,23 @@ describe "ESMs_system_territory_checkAccess", requires_connection: true, v2: tru
     expect(response.data.result).to eq(true)
   end
 
-  describe "Territory Admin" do
-    before :context do
-      before_connection do
-        community.update!(territory_admin_ids: [community.everyone_role_id])
-      end
-    end
+  it "returns true because the Player is a Territory Admin", :territory_admin_bypass do
+    territory.revoke_membership(user.steam_uid)
 
-    it "returns true because the Player is a Territory Admin" do
-      territory = ESM::ExileTerritory.all.active.with_no_membership_for(user).sampled_for(server)
+    response = execute_sqf!(
+      <<~SQF
+        private _territory = #{territory.id} call ESMs_system_territory_get;
+        [#{user.steam_uid.quoted}, _territory, "moderator"] call ESMs_system_territory_checkAccess
+      SQF
+    )
 
-      response = execute_sqf!(
-        <<~SQF
-          private _territory = #{territory.id} call ESMs_system_territory_get;
-          [#{user.steam_uid.quoted}, _territory, "moderator"] call ESMs_system_territory_checkAccess
-        SQF
-      )
-
-      expect(response).not_to be_nil
-      expect(response.data.result).to eq(true)
-    end
+    expect(response).not_to be_nil
+    expect(response.data.result).to eq(true)
   end
 
   it "returns false because the Player is missing rights" do
-    territory.revoke_membership(user)
-    
+    territory.revoke_membership(user.steam_uid)
+
     response = execute_sqf!(
       <<~SQF
         private _territory = #{territory.id} call ESMs_system_territory_get;
