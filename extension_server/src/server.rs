0a57701a678b391cb322ee9fe7a1de7f6a814e92
WolfkillArcadia <8569851+itsthedevman@users.noreply.github.com>
2023-05-29 15:29:09 -0400

    Fixed return message reusing inbound message

--- extension_server/src/server.rs
+++ extension_server/src/server.rs
@@ -86,7 +86,7 @@ async fn routing_thread(handler: Handler, mut receiver: UnboundedReceiver<Server
                     let Some(client) = client_manager.get_by_uuid(&server_uuid) else {
                         error!("[send] {} - Failed to retrieve client", server_uuid);
 
-                        let message = message.add_error_code("client_not_connected");
+                        let message = Message::new().set_id(message.id).add_error_code("client_not_connected");
                         if let Err(e) = BotRequest::send(message, Some(server_uuid)) {
                             error!("[send] {} - Failed to send error - {e}", server_uuid);
                         }
@@ -97,7 +97,7 @@ async fn routing_thread(handler: Handler, mut receiver: UnboundedReceiver<Server
                     if let Err(e) = client.send_message(&handler, message.as_mut()).await {
                         error!("{e}");
 
-                        let message = message.add_error_code("client_not_connected");
+                        let message = Message::new().set_id(message.id).add_error_code("client_not_connected");
                         if let Err(e) = BotRequest::send(message, Some(server_uuid)) {
                             error!("[send] {} - Failed to send error - {e}", server_uuid);
                         }
