8c91e3c6837910874229a79b77efa4449c0ff5f5
WolfkillArcadia <8569851+itsthedevman@users.noreply.github.com>
2023-07-08 21:25:31 -0400

    Load the bot when loading seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,12 @@
 require_relative "../lib/esm"
 require_relative "../spec/support/esm/test"
 
-ESM::Command.load
+ESM.console!
+ESM.run!
+
+until ESM.bot.ready?
+  sleep 1
+end
 
 ESM::BotAttribute.create!(
   maintenance_mode_enabled: false,
