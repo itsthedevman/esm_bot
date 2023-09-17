c2efde1f987a55b30997944726502f6d8fca3f5c
WolfkillArcadia <8569851+itsthedevman@users.noreply.github.com>
2023-09-17 16:59:18 -0400

    Migrated pay spec

--- spec/esm/command/server/pay_spec.rb
+++ spec/esm/command/server/pay_spec.rb
@@ -1,52 +1,26 @@
 # frozen_string_literal: true
 
 describe ESM::Command::Territory::Pay, category: "command" do
-  let!(:command) { ESM::Command::Territory::Pay.new }
-
-  it "should be valid" do
-    expect(command).not_to be_nil
-  end
-
-  it "should have 1 argument" do
-    expect(command.arguments.size).to eq(2)
-  end
-
-  it "should have a description" do
-    expect(command.description).not_to be_blank
-  end
-
-  it "should have examples" do
-    expect(command.example).not_to be_blank
-  end
+  include_context "command"
+  include_examples "validate_command"
 
   describe "#execute" do
-    let!(:community) { ESM::Test.community }
-    let!(:server) { ESM::Test.server }
-    let!(:user) { ESM::Test.user }
+    include_context "connection_v1"
 
-    let!(:wsc) { WebsocketClient.new(server) }
-    let(:connection) { ESM::Websocket.connections[server.server_id] }
+    context "when the execution is valid" do
+      it "returns a success message" do
+        request = execute!(
+          channel_type: :dm,
+          arguments: {server_id: server.server_id, territory_id: Faker::Crypto.md5[0, 5]}
+        )
 
-    before do
-      wait_for { wsc.connected? }.to be(true)
-    end
+        expect(request).not_to be_nil
+        wait_for { connection.requests }.to be_blank
+        wait_for { ESM::Test.messages.size }.to eq(1)
 
-    after do
-      wsc.disconnect!
-    end
-
-    it "should return" do
-      request = nil
-      command_statement = command.statement(server_id: server.server_id, territory_id: Faker::Crypto.md5[0, 5])
-      event = CommandEvent.create(command_statement, user: user, channel_type: :dm)
-
-      expect { request = command.execute(event) }.not_to raise_error
-      expect(request).not_to be_nil
-      wait_for { connection.requests }.to be_blank
-      wait_for { ESM::Test.messages.size }.to eq(1)
-
-      embed = ESM::Test.messages.first.second
-      expect(embed.description).to match(/has successfully received the payment/i)
+        embed = ESM::Test.messages.first.content
+        expect(embed.description).to match(/has successfully received the payment/i)
+      end
     end
   end
 end
