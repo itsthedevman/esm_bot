407b52d59b7bf6ff66465c8354885cbdaba45e78
WolfkillArcadia <8569851+itsthedevman@users.noreply.github.com>
2023-09-19 19:05:28 -0400

    Reward spec

--- spec/esm/command/server/reward_spec.rb
+++ spec/esm/command/server/reward_spec.rb
@@ -1,112 +1,79 @@
 # frozen_string_literal: true
 
 describe ESM::Command::Server::Reward, category: "command" do
-  let!(:command) { described_class.new }
-
-  it "should be valid" do
-    expect(command).not_to be_nil
-  end
-
-  it "should have 1 argument" do
-    expect(command.arguments.size).to eq(1)
-  end
-
-  it "should have a description" do
-    expect(command.description).not_to be_blank
-  end
-
-  it "should have examples" do
-    expect(command.example).not_to be_blank
-  end
+  include_context "command"
+  include_examples "validate_command"
 
   describe "#execute" do
-    let!(:community) { ESM::Test.community }
-    let!(:server) { ESM::Test.server }
-    let!(:user) { ESM::Test.user }
+    include_context "connection_v1"
 
-    let!(:wsc) { WebsocketClient.new(server) }
-    let(:connection) { ESM::Websocket.connections[server.server_id] }
-    let(:response) { command.response }
+    context "when there are rewards" do
+      it "gives them to the player and returns a message" do
+        execute!(arguments: {server_id: server.server_id})
+        wait_for { ESM::Test.messages.size }.to eq(2)
 
-    before do
-      wait_for { wsc.connected? }.to be(true)
-    end
+        embed = ESM::Test.messages.first.content
 
-    after do
-      wsc.disconnect!
-    end
+        # Checks for requestors message
+        expect(embed).not_to be_nil
 
-    it "!reward server_id" do
-      command_statement = command.statement(server_id: server.server_id)
-      event = CommandEvent.create(command_statement, user: user, channel_type: :text)
-      expect { command.execute(event) }.not_to raise_error
-      wait_for { ESM::Test.messages.size }.to eq(2)
+        # Process the request
+        request = command.request
+        expect(request).not_to be_nil
 
-      embed = ESM::Test.messages.first.second
+        # So we can track the response
+        ESM::Test.messages.clear
 
-      # Checks for requestors message
-      expect(embed).not_to be_nil
+        # Respond to the request
+        request.respond(true)
 
-      # Checks for requestees message
+        # Wait for the server to respond
+        wait_for { connection.requests }.to be_blank
+        wait_for { ESM::Test.messages.size }.to eq(1)
 
-      # Process the request
-      request = command.request
-      expect(request).not_to be_nil
+        embed = ESM::Test.messages.first.content
+        reward = server.server_reward
 
-      # So we can track the response
-      ESM::Test.messages.clear
+        expect(embed.description).to include("#{reward.player_poptabs}x Poptabs (Player)") if reward.player_poptabs.positive?
+        expect(embed.description).to include("#{reward.locker_poptabs}x Poptabs (Locker)") if reward.locker_poptabs.positive?
+        expect(embed.description).to include("#{reward.respect}x Respect") if reward.respect.positive?
 
-      # Respond to the request
-      request.respond(true)
-
-      # Wait for the server to respond
-      wait_for { connection.requests }.to be_blank
-
-      wait_for { ESM::Test.messages.size }.to eq(1)
-
-      embed = ESM::Test.messages.first.content
-
-      reward = server.server_reward
-
-      expect(embed.description).to include("#{reward.player_poptabs}x Poptabs (Player)") if reward.player_poptabs.positive?
-      expect(embed.description).to include("#{reward.locker_poptabs}x Poptabs (Locker)") if reward.locker_poptabs.positive?
-      expect(embed.description).to include("#{reward.respect}x Respect") if reward.respect.positive?
-
-      reward.reward_items.each do |item, quantity|
-        # Technically, the item should be converted to a proper display name by the server, but I don't have that ability here.
-        expect(embed.description).to include("#{quantity}x #{item}")
+        reward.reward_items.each do |item, quantity|
+          # Technically, the item should be converted to a proper display name by the server, but I don't have that ability here.
+          expect(embed.description).to include("#{quantity}x #{item}")
+        end
       end
     end
 
-    it "!reward server_id (Pending Request)" do
-      command_statement = command.statement(server_id: server.server_id)
-      event = CommandEvent.create(command_statement, user: user, channel_type: :text)
+    context "when the user already has a request" do
+      it "raises an exception" do
+        execution_args = {arguments: {server_id: server.server_id}}
 
-      # Create a pending request
-      ESM::Request.create!(
-        requestor_user_id: user.id,
-        requestee_user_id: user.id,
-        requested_from_channel_id: event.channel.id,
-        command_name: command.instance_variable_get(:@name),
-        command_arguments: {server_id: server.server_id}
-      )
+        # Create a pending request
+        create(
+          :request,
+          requestor: user,
+          requestee: user,
+          requested_from_channel_id: ESM::Test.channel.id,
+          command_name: command.command_name,
+          command_arguments: execution_args[:arguments]
+        )
 
-      expect { command.execute(event) }.to raise_error do |error|
-        embed = error.data
-
-        expect(embed.description).to match(/it appears you already have a request pending/i)
+        expect { execute!(**execution_args) }.to raise_error(ESM::Exception::CheckFailure) do |error|
+          embed = error.data
+          expect(embed.description).to match(/it appears you already have a request pending/i)
+        end
       end
     end
 
-    describe "No rewards" do
-      it "errors" do
+    context "when there are no rewards configured" do
+      it "raises an exception" do
         # Remove the default reward and create a blank one
         server.server_reward.delete
         server.send(:create_default_reward)
 
-        command_statement = command.statement(server_id: server.server_id)
-        event = CommandEvent.create(command_statement, user: user, channel_type: :text)
-        expect { command.execute(event) }.to raise_error do |error|
+        execution_args = {arguments: {server_id: server.server_id}}
+        expect { execute!(**execution_args) }.to raise_error(ESM::Exception::CheckFailure) do |error|
           embed = error.data
           expect(embed.description).to match(/the selected reward package is not available at this tim/i)
         end
