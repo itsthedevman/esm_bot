a7910d1d4d3a8733c6b537aa76ddc32e08047f04
WolfkillArcadia <bryan@itsthedevman.com>
2023-09-23 18:33:53 -0400

    requests spec

--- spec/esm/command/system/requests_spec.rb
+++ spec/esm/command/system/requests_spec.rb
@@ -1,45 +1,36 @@
+# frozen_string_literal: true
+
 describe ESM::Command::Request::Requests, category: "command" do
-  let!(:command) { described_class.new }
-
-  it "is valid" do
-    expect(command).not_to be_nil
-  end
-
-  it "has 0 arguments" do
-    expect(command.arguments.size).to eq(0)
-  end
-
-  it "has a description" do
-    expect(command.description).not_to be_blank
-  end
-
-  it "has examples" do
-    expect(command.example).not_to be_blank
-  end
+  include_context "command"
+  include_examples "validate_command"
 
   describe "#execute" do
-    let!(:user) { ESM::Test.user }
-    let!(:second_user) { ESM::Test.user }
-    let!(:reward_request) { create(:request, requestor_user_id: user.id, requestee_user_id: user.id) }
-    let!(:add_request) { create(:request, requestor_user_id: second_user.id, requestee_user_id: user.id, command_name: "add") }
+    let!(:reward_request) do
+      create(:request, requestor_user_id: user.id, requestee_user_id: user.id)
+    end
 
-    it "!requests" do
-      event = CommandEvent.create(command.statement, user: user, channel_type: :dm)
-      expect { command.execute(event) }.not_to raise_error
+    let!(:add_request) do
+      create(:request, requestor_user_id: second_user.id, requestee_user_id: user.id, command_name: "add")
+    end
 
-      embed = ESM::Test.messages.first.second
-      expect(embed).not_to be(nil)
-      expect(embed.title).to eq("Pending Requests")
+    context "when the user has requests" do
+      it "returns a list of them" do
+        execute!(channel_type: :dm)
 
-      expect(embed.description).not_to include(reward_request.requestor.distinct)
-      expect(embed.description).to include(
-        "reward", reward_request.expires_at.to_s,
-        "[Accept]", "~accept #{reward_request.uuid_short}",
-        "[Decline]", "~decline #{reward_request.uuid_short}",
-        "add", add_request.requestor.distinct, add_request.expires_at.to_s,
-        "[Accept]", "~accept #{add_request.uuid_short}",
-        "[Decline]", "~decline #{add_request.uuid_short}"
-      )
+        embed = ESM::Test.messages.first.content
+        expect(embed).not_to be(nil)
+        expect(embed.title).to eq("Pending Requests")
+
+        expect(embed.description).not_to include(reward_request.requestor.distinct)
+        expect(embed.description).to include(
+          "reward", reward_request.expires_at.to_s,
+          "[Accept]", "/requests accept uuid:#{reward_request.uuid_short}",
+          "[Decline]", "/requests decline uuid:#{reward_request.uuid_short}",
+          "add", add_request.requestor.distinct, add_request.expires_at.to_s,
+          "[Accept]", "/requests accept uuid:#{add_request.uuid_short}",
+          "[Decline]", "/requests decline uuid:#{add_request.uuid_short}"
+        )
+      end
     end
   end
 end
