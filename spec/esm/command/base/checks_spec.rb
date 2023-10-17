b0084b3cf1e2ffca8a2bdc98d5fa575e951f4a84
Bryan <bryan@itsthedevman.com>
2023-10-17 17:15:34 -0400

    Checks spec

--- spec/esm/command/base/checks_spec.rb
+++ spec/esm/command/base/checks_spec.rb
@@ -1,39 +1,39 @@
 # frozen_string_literal: true
 
 describe ESM::Command::Base::Checks do
-  let!(:command) { ESM::Command::Test::TargetCommand.new }
-  let!(:community) { ESM::Test.community }
-  let!(:user) { ESM::Test.user }
-  let!(:second_user) { ESM::Test.user }
+  include_context "command" do
+    let!(:command_class) { ESM::Command::Test::TargetCommand }
+  end
 
   before do
-    command.instance_variable_set(:@current_user, user.discord_user)
+    command.current_user = user
+    command.current_channel = ESM::Test.channel
   end
 
   describe "#registered_target_user!" do
-    it "should not raise (target_user is nil)" do
+    it "does not raise (target_user is nil)" do
       command.instance_variable_set(:@target_user, nil)
-      expect { command.check_registered_target_user! }.not_to raise_error
+      expect { command.check_for_registered_target_user! }.not_to raise_error
     end
 
-    it "should not raise (target_user is registered)" do
-      command.instance_variable_set(:@target_user, user.discord_user)
-      expect { command.check_registered_target_user! }.not_to raise_error
+    it "does not raise (target_user is registered)" do
+      command.instance_variable_set(:@target_user, user)
+      expect { command.check_for_registered_target_user! }.not_to raise_error
     end
 
-    it "should raise (target_user is of type User::Ephemeral)" do
+    it "raises (target_user is of type User::Ephemeral)" do
       steam_uid = second_user.steam_uid
       second_user.destroy
 
       command.instance_variable_set(:@target_user, ESM::User::Ephemeral.new(steam_uid))
-      expect { command.check_registered_target_user! }.to raise_error(ESM::Exception::CheckFailure, /has not registered with me yet/i)
+      expect { command.check_for_registered_target_user! }.to raise_error(ESM::Exception::CheckFailure, /has not registered with me yet/i)
     end
 
-    it "should raise (target_user is not registered)" do
+    it "raises (target_user is not registered)" do
       second_user.update!(steam_uid: "")
 
       command.instance_variable_set(:@target_user, second_user)
-      expect { command.check_registered_target_user! }.to raise_error(ESM::Exception::CheckFailure, /has not registered with me yet/i)
+      expect { command.check_for_registered_target_user! }.to raise_error(ESM::Exception::CheckFailure, /has not registered with me yet/i)
     end
   end
 end
