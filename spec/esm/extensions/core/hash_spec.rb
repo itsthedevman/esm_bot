db7548d3952a3aa48e73ac085067958ec7e588e0
Bryan <bryan@itsthedevman.com>
2024-03-02 15:47:12 -0500

    Hash to_struct method cleanup

--- spec/esm/extensions/core/hash_spec.rb
+++ spec/esm/extensions/core/hash_spec.rb
@@ -1,6 +1,32 @@
 # frozen_string_literal: true
 
 describe Hash do
+  describe "#to_struct" do
+    it "converts to Struct" do
+      obj = {foo: "bar"}.to_struct
+      expect(obj).not_to be_nil
+      expect(obj).to be_a(Struct)
+      expect(obj.foo).to eq("bar")
+    end
+
+    it "is recursive" do
+      obj = {foo: {bar: {baz: true}}}.to_struct
+      expect(obj).not_to be_nil
+      expect(obj).to be_a(Struct)
+      expect(obj.foo).to be_a(Struct)
+      expect(obj.foo.bar).to be_a(Struct)
+      expect(obj.foo.bar.baz).to be(true)
+    end
+
+    it "preserves ruby classes" do
+      input = {time: Time.current}
+      output = input.to_struct
+      expect(output).not_to be_nil
+      expect(output).to be_a(Struct)
+      expect(output.time).to eq(input[:time])
+    end
+  end
+
   describe "#to_ostruct" do
     it "converts to OpenStruct" do
       obj = {foo: "bar"}.to_ostruct
