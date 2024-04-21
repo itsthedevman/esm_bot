7e6a0e5a0012b56c8ab399d26a6e7d928ef90709
Bryan <bryan@itsthedevman.com>
2024-04-21 15:12:52 -0400

    Check writable and close if errors

--- lib/esm/connection/client_socket.rb
+++ lib/esm/connection/client_socket.rb
@@ -17,10 +17,13 @@ module ESM
 
       def write(data)
         raise TypeError, "Expected String, got #{data.class}" unless data.is_a?(String)
+        return unless writeable?
 
         __getobj__.send(data, 0)
       rescue TypeError
         raise
+      rescue Errno::EPIPE
+        close
       rescue => e
         error!(error: e)
       end
