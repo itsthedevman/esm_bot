ec739c0876c24d715da474bc11b9219d476c59f1
Bryan <bryan@itsthedevman.com>
2024-10-19 16:19:34 -0400

    Added xm8 notification spec model

---
+++ spec/support/extensions/esm/model/exile_xm8_notification.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module ESM
+  class ExileXm8Notification < ArmaRecord
+    self.table_name = "xm8_notification"
+
+    attribute :uuid, :string, default: -> { SecureRandom.uuid }
+    attribute :recipient_uid, :string
+    attribute :territory_id, :integer, default: nil
+    attribute :type, :string
+    attribute :content, :json
+    attribute :status, :string, default: "new"
+    attribute :attempt_count, :integer, default: 0
+    attribute :created_at, :datetime, default: -> { Time.current }
+    attribute :last_attempt_at, :datetime
+    attribute :acknowledged_at, :datetime
+
+    belongs_to :account,
+      class_name: "ESM::ExileAccount",
+      foreign_key: "recipient_uid"
+
+    belongs_to :territory,
+      class_name: "ESM::ExileTerritory",
+      foreign_key: "territory_id",
+      optional: true
+  end
+end
