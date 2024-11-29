4bc567ed5e5dc8f9e3e99ec86d192c62b3169b8c
Bryan <bryan@itsthedevman.com>
2024-11-29 00:30:34 -0500

    Initial

--- lib/esm/command/server/info.rb
+++ lib/esm/command/server/info.rb
@@ -39,39 +39,114 @@ module ESM
           # Ensure we were given a target or territory ID
           check_for_no_target!
 
-          # Territory ID takes priority since both arguments are optional, and both can be provided
-          # Reasoning: Given the weird scenario, user gives a mention plus a territory ID thinking that it will do some filter...
-          if arguments.territory_id.present?
-            deliver!(query: "territory_info", territory_id: arguments.territory_id)
-          else
-            deliver!(query: "player_info", uid: target_user.steam_uid)
-          end
-        end
+          # Territory ID takes priority since both arguments are optional
+          # and both can be provided
+          #
+          # Reasoning:
+          #   Given the weird scenario, user gives a mention plus a territory ID thinking
+          #   that it will do some filter...
+          embed =
+            if arguments.territory_id.present?
+              query_for_territory_info!
+            else
+              query_for_player_info!
+            end
 
-        def on_response
-          # I'm not quite sure if this is needed, but just in case...
-          check_for_response!
-
-          # The fact that it has an ID indicates it was a territory...
-          if @response.id
-            territory = ESM::Exile::Territory.new(server: target_server, territory: @response)
-            reply(territory.to_embed)
-          else
-            player = ESM::Exile::Player.new(server: target_server, player_data: @response)
-            reply(player.to_embed)
-          end
+          reply(embed)
         end
 
         private
 
-        def check_for_no_target!
-          raise_error!(:no_target, user: current_user.mention) if arguments.target.blank? && arguments.territory_id.blank?
+        def query_for_territory_info!
+          territory = query_exile_database!(
+            "territory_info",
+            territory_id: arguments.territory_id
+          ).first
+
+          check_for_territory_info!(territory)
+
+          territory = ESM::Exile::Territory.new(
+            server: target_server,
+            territory: territory.to_istruct
+          )
+
+          territory.to_embed
         end
 
-        def check_for_response!
-          return if @response.present?
+        def query_for_player_info!
+          player = query_exile_database!(
+            "player_info",
+            uid: target_user.steam_uid
+          ).first
 
-          raise_error!(:no_response, user: current_user.mention)
+          check_for_player_info!(player)
+
+          player = ESM::Exile::Player.new(server: target_server, player: player.to_istruct)
+
+          player.to_embed
+        end
+
+        def check_for_no_target!
+          return unless arguments.target.blank? && arguments.territory_id.blank?
+
+          raise_error!(:no_target, user: current_user.mention)
+        end
+
+        def check_for_player_info!(result)
+          return if result.present?
+
+          raise_error!(
+            :no_player_info,
+            user: current_user.mention,
+            target: arguments.target
+          )
+        end
+
+        def check_for_territory_info!(result)
+          return if result.present?
+
+          raise_error!(
+            :no_territory_info,
+            user: current_user.mention,
+            territory_id: arguments.territory_id
+          )
+        end
+
+        module V1
+          def on_execute
+            # Ensure we were given a target or territory ID
+            check_for_no_target!
+
+            # Territory ID takes priority since both arguments are optional, and both can be provided
+            # Reasoning: Given the weird scenario, user gives a mention plus a territory ID thinking that it will do some filter...
+            if arguments.territory_id.present?
+              deliver!(query: "territory_info", territory_id: arguments.territory_id)
+            else
+              deliver!(query: "player_info", uid: target_user.steam_uid)
+            end
+          end
+
+          def on_response
+            # I'm not quite sure if this is needed, but just in case...
+            check_for_response!
+
+            # The fact that it has an ID indicates it was a territory...
+            if @response.id
+              territory = ESM::Exile::Territory.new(server: target_server, territory: @response)
+              reply(territory.to_embed)
+            else
+              player = ESM::Exile::Player.new(server: target_server, player: @response)
+              reply(player.to_embed)
+            end
+          end
+
+          private
+
+          def check_for_response!
+            return if @response.present?
+
+            raise_error!(:no_response, user: current_user.mention)
+          end
         end
       end
     end
--- spec/esm/command/server/info_spec.rb
+++ spec/esm/command/server/info_spec.rb
@@ -4,136 +4,191 @@ describe ESM::Command::Server::Info, category: "command" do
   include_context "command"
   include_examples "validate_command"
 
-  describe "#execute" do
-    include_context "connection_v1"
+  it "is an admin command" do
+    expect(command.type).to eq(:admin)
+  end
 
-    before do
-      grant_command_access!(community, "info")
+  before do
+    grant_command_access!(community, "info")
+  end
 
-      # IMPORTANT!
-      # If we don't reload the server model after WSC connection, the server settings will be wrong!
-      server.reload
-    end
+  describe "V1" do
+    describe "#execute" do
+      include_context "connection_v1"
 
-    context "when the mentioned target is alive" do
-      it "returns information on the player" do
-        wsc.flags.PLAYER_ALIVE = true
-
-        request = execute!(arguments: {server_id: server.server_id, target: user.mention})
-        expect(request).not_to be_nil
-        wait_for { connection.requests }.to be_blank
-        wait_for { ESM::Test.messages.size }.to eq(1)
-
-        embed = ESM::Test.messages.first.content
-        expect(embed.title).to match(/stats on `#{server.server_id}`/i)
-
-        field = embed.fields.first
-        expect(field.name).to match(/general/i)
-        expect(field.value).to match(/health.+%.+hunger.+%.+thirst.+%/im)
-
-        field = embed.fields.second
-        expect(field.name).to match(/currency/i)
-        expect(field.value).to match(/money.+poptabs.+locker.+poptabs.+respect.+/im)
-
-        field = embed.fields.third
-        expect(field.name).to match(/scoreboard/i)
-        expect(field.value).to match(/kills.+deaths.+kd ratio.+/im)
+      before do
+        # IMPORTANT!
+        # If we don't reload the server model after WSC connection, the server settings will be wrong!
+        server.reload
       end
-    end
 
-    context "when the steam uid target is a dead player" do
-      it "returns information on the player" do
-        request = execute!(arguments: {server_id: server.server_id, target: user.steam_uid})
-        expect(request).not_to be_nil
-        wait_for { connection.requests }.to be_blank
-        wait_for { ESM::Test.messages.size }.to eq(1)
+      context "when the mentioned target is alive" do
+        it "returns information on the player" do
+          wsc.flags.PLAYER_ALIVE = true
 
-        embed = ESM::Test.messages.first.content
-        expect(embed.title).to match(/stats on `#{server.server_id}`/i)
+          request = execute!(arguments: {server_id: server.server_id, target: user.mention})
+          expect(request).not_to be_nil
+          wait_for { connection.requests }.to be_blank
+          wait_for { ESM::Test.messages.size }.to eq(1)
 
-        field = embed.fields.first
-        expect(field.name).to match(/general/i)
-        expect(field.value).to match("**You are dead**")
+          embed = ESM::Test.messages.first.content
+          expect(embed.title).to match(/stats on `#{server.server_id}`/i)
 
-        field = embed.fields.second
-        expect(field.name).to match(/currency/i)
-        expect(field.value).to match(/money.+you are dead.+locker.+poptabs.+respect.+/im)
+          field = embed.fields.first
+          expect(field.name).to match(/general/i)
+          expect(field.value).to match(/health.+%.+hunger.+%.+thirst.+%/im)
 
-        field = embed.fields.third
-        expect(field.name).to match(/scoreboard/i)
-        expect(field.value).to match(/kills.+deaths.+kd ratio.+/im)
-      end
-    end
+          field = embed.fields.second
+          expect(field.name).to match(/currency/i)
+          expect(field.value).to match(/money.+poptabs.+locker.+poptabs.+respect.+/im)
 
-    context "when the target is a territory ID" do
-      it "returns information on the territory" do
-        request = execute!(arguments: {server_id: server.server_id, territory_id: "12345"})
-        expect(request).not_to be_nil
-        wait_for { connection.requests }.to be_blank
-        wait_for { ESM::Test.messages.size }.to eq(1)
-
-        embed = ESM::Test.messages.first.content
-        territory = ESM::Exile::Territory.new(server: server, territory: response)
-
-        expect(embed.title).to eq("Territory \"#{territory.name}\"")
-        expect(embed.thumbnail.url).to eq(territory.flag_path)
-        expect(embed.color).to eq(territory.status_color)
-        expect(embed.description).to eq(territory.payment_reminder_message)
-
-        field_info = [
-          {name: "Territory ID", value: "```#{territory.id}```"},
-          {name: "Flag Status", value: "```#{territory.flag_status}```"},
-          {name: "Next Due Date", value: "```#{territory.next_due_date.strftime(ESM::Time::Format::TIME)}```"},
-          {name: "Last Paid", value: "```#{territory.last_paid_at.strftime(ESM::Time::Format::TIME)}```"},
-          {name: "Price to renew protection", value: territory.renew_price},
-          {value: "__Current Territory Stats__"},
-          {name: "Level", value: territory.level},
-          {name: "Radius", value: "#{territory.radius}m"},
-          {name: "Current / Max Objects", value: "#{territory.object_count}/#{territory.max_object_count}"}
-        ]
-
-        if territory.upgradeable?
-          field_info.push(
-            {value: "__Next Territory Stats__"},
-            {name: "Level", value: territory.upgrade_level},
-            {name: "Radius", value: "#{territory.upgrade_radius}m"},
-            {name: "Max Objects", value: territory.upgrade_object_count},
-            {name: "Price", value: territory.upgrade_price}
-          )
-        end
-
-        field_info.push({value: "__Territory Members__"}, {name: ":crown: Owner", value: territory.owner})
-
-        # Now check the fields
-        # Removing them from the embed so we can check moderators/builders easily
-        field_info.each do |field|
-          embed_field = embed.fields.shift
-          expect(embed_field.name).to eq(field[:name].to_s) if field[:name].present?
-          expect(embed_field.value).to eq(field[:value].to_s)
-        end
-
-        moderator_fields = ESM::Embed.new
-          .add_field(value: territory.moderators)
-          .fields
-          .map(&:value)
-
-        moderator_fields.each do |moderator_field|
-          field = embed.fields.shift
-          expect(field.name).to match(/moderator/i)
-          expect(field.value).to eq(moderator_field)
-        end
-
-        builder_fields = ESM::Embed.new
-          .add_field(value: territory.builders)
-          .fields
-          .map(&:value)
-
-        builder_fields.each do |builder_field|
-          field = embed.fields.shift
-          expect(field.name).to match(/builder/i)
-          expect(field.value).to eq(builder_field)
+          field = embed.fields.third
+          expect(field.name).to match(/scoreboard/i)
+          expect(field.value).to match(/kills.+deaths.+kd ratio.+/im)
         end
       end
+
+      context "when the steam uid target is a dead player" do
+        it "returns information on the player" do
+          request = execute!(arguments: {server_id: server.server_id, target: user.steam_uid})
+          expect(request).not_to be_nil
+          wait_for { connection.requests }.to be_blank
+          wait_for { ESM::Test.messages.size }.to eq(1)
+
+          embed = ESM::Test.messages.first.content
+          expect(embed.title).to match(/stats on `#{server.server_id}`/i)
+
+          field = embed.fields.first
+          expect(field.name).to match(/general/i)
+          expect(field.value).to match("**You are dead**")
+
+          field = embed.fields.second
+          expect(field.name).to match(/currency/i)
+          expect(field.value).to match(/money.+you are dead.+locker.+poptabs.+respect.+/im)
+
+          field = embed.fields.third
+          expect(field.name).to match(/scoreboard/i)
+          expect(field.value).to match(/kills.+deaths.+kd ratio.+/im)
+        end
+      end
+
+      context "when the target is a territory ID" do
+        it "returns information on the territory" do
+          request = execute!(arguments: {server_id: server.server_id, territory_id: "12345"})
+          expect(request).not_to be_nil
+          wait_for { connection.requests }.to be_blank
+          wait_for { ESM::Test.messages.size }.to eq(1)
+
+          embed = ESM::Test.messages.first.content
+          territory = ESM::Exile::Territory.new(server: server, territory: response)
+
+          expect(embed.title).to eq("Territory \"#{territory.name}\"")
+          expect(embed.thumbnail.url).to eq(territory.flag_path)
+          expect(embed.color).to eq(territory.status_color)
+          expect(embed.description).to eq(territory.payment_reminder_message)
+
+          field_info = [
+            {name: "Territory ID", value: "```#{territory.id}```"},
+            {name: "Flag Status", value: "```#{territory.flag_status}```"},
+            {name: "Next Due Date", value: "```#{territory.next_due_date.strftime(ESM::Time::Format::TIME)}```"},
+            {name: "Last Paid", value: "```#{territory.last_paid_at.strftime(ESM::Time::Format::TIME)}```"},
+            {name: "Price to renew protection", value: territory.renew_price},
+            {value: "__Current Territory Stats__"},
+            {name: "Level", value: territory.level},
+            {name: "Radius", value: "#{territory.radius}m"},
+            {name: "Current / Max Objects", value: "#{territory.object_count}/#{territory.max_object_count}"}
+          ]
+
+          if territory.upgradeable?
+            field_info.push(
+              {value: "__Next Territory Stats__"},
+              {name: "Level", value: territory.upgrade_level},
+              {name: "Radius", value: "#{territory.upgrade_radius}m"},
+              {name: "Max Objects", value: territory.upgrade_object_count},
+              {name: "Price", value: territory.upgrade_price}
+            )
+          end
+
+          field_info.push({value: "__Territory Members__"}, {name: ":crown: Owner", value: territory.owner})
+
+          # Now check the fields
+          # Removing them from the embed so we can check moderators/builders easily
+          field_info.each do |field|
+            embed_field = embed.fields.shift
+            expect(embed_field.name).to eq(field[:name].to_s) if field[:name].present?
+            expect(embed_field.value).to eq(field[:value].to_s)
+          end
+
+          moderator_fields = ESM::Embed.new
+            .add_field(value: territory.moderators)
+            .fields
+            .map(&:value)
+
+          moderator_fields.each do |moderator_field|
+            field = embed.fields.shift
+            expect(field.name).to match(/moderator/i)
+            expect(field.value).to eq(moderator_field)
+          end
+
+          builder_fields = ESM::Embed.new
+            .add_field(value: territory.builders)
+            .fields
+            .map(&:value)
+
+          builder_fields.each do |builder_field|
+            field = embed.fields.shift
+            expect(field.name).to match(/builder/i)
+            expect(field.value).to eq(builder_field)
+          end
+        end
+      end
+    end
+  end
+
+  describe "V2" do
+    describe "#on_execute", requires_connection: true do
+      include_context "connection"
+
+      let(:target) { user.mention }
+
+      subject(:execute_command) { execute!(arguments: {server_id: server.server_id, target:}) }
+
+      context "when the target is a player" do
+        context "and the player is alive" do
+          before do
+            spawn_player_for(user)
+          end
+
+          it "is expected to return information on the player" do
+            execute_command
+
+            wait_for { ESM::Test.messages.size }.to eq(1)
+
+            embed = latest_message
+            expect(embed.title).to match(/stats on `#{server.server_id}`/i)
+
+            field = embed.fields.first
+            expect(field.name).to match(/general/i)
+            expect(field.value).to match(/health.+%.+hunger.+%.+thirst.+%/im)
+
+            field = embed.fields.second
+            expect(field.name).to match(/currency/i)
+            expect(field.value).to match(/money.+poptabs.+locker.+poptabs.+respect.+/im)
+
+            field = embed.fields.third
+            expect(field.name).to match(/scoreboard/i)
+            expect(field.value).to match(/kills.+deaths.+kd ratio.+/im)
+          end
+        end
+
+        context "and the player is dead"
+      end
+
+      context "when the target is a steam uid"
+      context "when the target has not joined the server"
+
+      context "when the target is a territory"
+      context "when the target is not a valid territory"
     end
   end
 end
