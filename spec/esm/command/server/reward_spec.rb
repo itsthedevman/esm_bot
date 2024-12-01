0040ecd8c16c5d3c64d91b254617e5787b381128
Bryan <bryan@itsthedevman.com>
2024-11-30 22:14:01 -0500

    Initial

--- spec/esm/command/server/reward_spec.rb
+++ spec/esm/command/server/reward_spec.rb
@@ -4,78 +4,114 @@ describe ESM::Command::Server::Reward, category: "command" do
   include_context "command"
   include_examples "validate_command"
 
-  describe "#execute" do
-    include_context "connection_v1"
+  it "is a player command" do
+    expect(command.type).to eq(:player)
+  end
 
-    context "when there are rewards" do
-      it "gives them to the player and returns a message" do
-        execute!(arguments: {server_id: server.server_id})
-        wait_for { ESM::Test.messages.size }.to eq(2)
+  describe "V1" do
+    describe "#execute" do
+      include_context "connection_v1"
 
-        embed = ESM::Test.messages.first.content
+      context "when there are rewards" do
+        it "gives them to the player and returns a message" do
+          execute!(arguments: {server_id: server.server_id})
+          wait_for { ESM::Test.messages.size }.to eq(2)
 
-        # Checks for requestors message
-        expect(embed).not_to be_nil
+          embed = ESM::Test.messages.first.content
 
-        # Process the request
-        request = previous_command.request
-        expect(request).not_to be_nil
+          # Checks for requestors message
+          expect(embed).not_to be_nil
 
-        # So we can track the response
-        ESM::Test.messages.clear
+          # Process the request
+          request = previous_command.request
+          expect(request).not_to be_nil
 
-        # Respond to the request
-        request.respond(true)
+          # So we can track the response
+          ESM::Test.messages.clear
 
-        # Wait for the server to respond
-        wait_for { connection.requests }.to be_blank
-        wait_for { ESM::Test.messages.size }.to eq(1)
+          # Respond to the request
+          request.respond(true)
 
-        embed = ESM::Test.messages.first.content
-        reward = server.server_reward
+          # Wait for the server to respond
+          wait_for { connection.requests }.to be_blank
+          wait_for { ESM::Test.messages.size }.to eq(1)
 
-        expect(embed.description).to include("#{reward.player_poptabs}x Poptabs (Player)") if reward.player_poptabs.positive?
-        expect(embed.description).to include("#{reward.locker_poptabs}x Poptabs (Locker)") if reward.locker_poptabs.positive?
-        expect(embed.description).to include("#{reward.respect}x Respect") if reward.respect.positive?
+          embed = ESM::Test.messages.first.content
+          reward = server.server_reward
 
-        reward.reward_items.each do |item, quantity|
-          # Technically, the item should be converted to a proper display name by the server, but I don't have that ability here.
-          expect(embed.description).to include("#{quantity}x #{item}")
+          expect(embed.description).to include("#{reward.player_poptabs}x Poptabs (Player)") if reward.player_poptabs.positive?
+          expect(embed.description).to include("#{reward.locker_poptabs}x Poptabs (Locker)") if reward.locker_poptabs.positive?
+          expect(embed.description).to include("#{reward.respect}x Respect") if reward.respect.positive?
+
+          reward.reward_items.each do |item, quantity|
+            # Technically, the item should be converted to a proper display name by the server, but I don't have that ability here.
+            expect(embed.description).to include("#{quantity}x #{item}")
+          end
+        end
+      end
+
+      context "when the user already has a request" do
+        it "raises an exception" do
+          execution_args = {arguments: {server_id: server.server_id}}
+
+          # Create a pending request
+          create(
+            :request,
+            requestor: user,
+            requestee: user,
+            requested_from_channel_id: ESM::Test.channel(in: community).id,
+            command_name: command.command_name,
+            command_arguments: execution_args[:arguments]
+          )
+
+          expect { execute!(**execution_args) }.to raise_error(ESM::Exception::CheckFailure) do |error|
+            embed = error.data
+            expect(embed.description).to match(/it appears you already have a request pending/i)
+          end
+        end
+      end
+
+      context "when there are no rewards configured" do
+        it "raises an exception" do
+          # Remove the default reward and create a blank one
+          server.server_reward.delete
+          server.send(:create_default_reward)
+
+          execution_args = {arguments: {server_id: server.server_id}}
+          expect { execute!(**execution_args) }.to raise_error(ESM::Exception::CheckFailure) do |error|
+            embed = error.data
+            expect(embed.description).to match(/the selected reward package is not available at this tim/i)
+          end
         end
       end
     end
+  end
 
-    context "when the user already has a request" do
-      it "raises an exception" do
-        execution_args = {arguments: {server_id: server.server_id}}
+  describe "V2", v2: true do
+    describe "#on_execute", requires_connection: true do
+      include_context "connection"
 
-        # Create a pending request
-        create(
-          :request,
-          requestor: user,
-          requestee: user,
-          requested_from_channel_id: ESM::Test.channel(in: community).id,
-          command_name: command.command_name,
-          command_arguments: execution_args[:arguments]
-        )
+      subject(:execute_command) { execute!(arguments: {server_id: server.server_id}) }
 
-        expect { execute!(**execution_args) }.to raise_error(ESM::Exception::CheckFailure) do |error|
-          embed = error.data
-          expect(embed.description).to match(/it appears you already have a request pending/i)
-        end
+      before do
+        spawn_player_for(user)
       end
-    end
 
-    context "when there are no rewards configured" do
-      it "raises an exception" do
-        # Remove the default reward and create a blank one
-        server.server_reward.delete
-        server.send(:create_default_reward)
+      context "when there are rewards" do
+      end
 
-        execution_args = {arguments: {server_id: server.server_id}}
-        expect { execute!(**execution_args) }.to raise_error(ESM::Exception::CheckFailure) do |error|
-          embed = error.data
-          expect(embed.description).to match(/the selected reward package is not available at this tim/i)
+      context "when there are no rewards" do
+        before do
+          server.server_reward.update!(
+            reward_items: [],
+            player_poptabs: 0,
+            locker_poptabs: 0,
+            respect: 0
+          )
+        end
+
+        include_examples "raises_check_failure" do
+          let!(:matcher) { "the selected reward package is not available at this time" }
         end
       end
     end
