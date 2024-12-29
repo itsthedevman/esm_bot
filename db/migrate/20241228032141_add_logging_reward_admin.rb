e44d4a2d4b4f1d8b14719dd12d9c1e2e5a39b2ef
Bryan <bryan@itsthedevman.com>
2024-12-28 23:54:52 -0500

    Fixed typo in migration

--- db/migrate/20241228032141_add_logging_reward_admin.rb
+++ db/migrate/20241228032141_add_logging_reward_admin.rb
@@ -1,5 +1,5 @@
 class AddLoggingRewardAdmin < ActiveRecord::Migration[7.2]
   def change
-    add_column(:server_setting, :logging_reward_admin, :boolean, default: true, if_not_exists: true)
+    add_column(:server_settings, :logging_reward_admin, :boolean, default: true, if_not_exists: true)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_12_05_054436) do
+ActiveRecord::Schema[7.2].define(version: 2024_12_28_032141) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
   enable_extension "plpgsql"
@@ -257,6 +257,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_12_05_054436) do
     t.string "number_locale"
     t.integer "exile_logs_search_days"
     t.json "additional_logs", default: []
+    t.boolean "logging_reward_admin", default: true
     t.index ["deleted_at"], name: "index_server_settings_on_deleted_at"
     t.index ["server_id"], name: "index_server_settings_on_server_id"
   end
