b6d3da365a3f56819f05c1618db50d67d4dd77fa
Bryan <bryan@itsthedevman.com>
2024-12-29 21:28:45 -0500

    Initial

--- lib/esm/command/server/reward.rb
+++ lib/esm/command/server/reward.rb
@@ -25,10 +25,19 @@ module ESM
 
         def on_execute
           # Check to see if the provided reward_id has anything to redeem
+          check_for_reward_items!
+
           # Check to see if the user is on cooldown for the reward
-          # Show the user what they're going to redeem, for how long, how to redeem in game
-          # and prompt for confirmation.
+          check_for_reward_cooldown!
+
+          # Confirm with the player
+          confirmed = prompt_for_confirmation!(confirmation_embed)
+
+          nil unless confirmed
+
           # Add them items to the user's rewards
+          # Update their cooldown
+          # Let them know!
         end
 
         private
@@ -36,6 +45,70 @@ module ESM
         def selected_reward
           @selected_reward ||= target_server.server_rewards.find_by(reward_id: arguments.reward_id)
         end
+
+        def reward_cooldown
+          # Luckily, the standard command cooldown query basically 90% of the way there
+          @reward_cooldown ||=
+            current_cooldown_query.rewhere(type: :reward, key: selected_reward.reward_id)
+              .first_or_create!
+        end
+
+        def check_for_reward_items!
+          return if selected_reward.items.size > 0
+
+          raise_error!(:no_reward_items, user: current_user.mention)
+        end
+
+        def check_for_reward_cooldown!
+          return unless reward_cooldown.active?
+
+          if current_cooldown.cooldown_type == Cooldown::COOLDOWN_TYPE_TIMES
+            raise_error!(
+              :on_cooldown_useage,
+              user: current_user.mention,
+              command_name: usage
+            )
+          end
+
+          raise_error!(
+            :on_cooldown_time_left,
+            user: current_user.mention,
+            time_left: current_cooldown.to_s,
+            command_name: usage
+          )
+        end
+
+        def confirmation_embed
+          ESM::Embed.build do |e|
+            e.title = translate("confirmation.title")
+
+            expiry =
+              if duration
+                translate(
+                  "expiry.timed",
+                  duration: ChronicDuration.output(duration)
+                )
+              else
+                translate("expiry.never")
+              end
+
+            e.description = translate(
+              "confirmation.description",
+              recipient: target_user.discord_mention,
+              type: arguments.type.titleize,
+              reward_details: translate(
+                "reward_details.#{arguments.type}",
+                amount: arguments.amount.to_delimitated_s,
+                name: display_name
+              ),
+              expiry:,
+              recipient_mention: target_user.discord_mention,
+              server_id: target_server.server_id
+            )
+          end
+        end
+
+        ########################################################################
 
         module V1
           def on_execute
