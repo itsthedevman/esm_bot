a1f78f9d392b23a92da833d0bb73330384895e1b
Bryan <bryan@itsthedevman.com>
2024-12-31 02:10:29 -0500

    Const rename

--- lib/esm/model/concerns/cooldownable.rb
+++ lib/esm/model/concerns/cooldownable.rb
@@ -6,25 +6,23 @@ module ESM
       extend ActiveSupport::Concern
 
       COOLDOWN_TYPES = [
-        COOLDOWN_TYPE_NEVER = "never",
-        COOLDOWN_TYPE_TIMES = "times",
-        COOLDOWN_TYPE_SECONDS = "seconds",
-        COOLDOWN_TYPE_MINUTES = "minutes",
-        COOLDOWN_TYPE_HOURS = "hours",
-        COOLDOWN_TYPE_DAYS = "days",
-        COOLDOWN_TYPE_WEEKS = "weeks",
-        COOLDOWN_TYPE_MONTHS = "months",
-        COOLDOWN_TYPE_YEARS = "years"
+        COOLDOWN_TIMES = "times",
+        COOLDOWN_SECONDS = "seconds",
+        COOLDOWN_MINUTES = "minutes",
+        COOLDOWN_HOURS = "hours",
+        COOLDOWN_DAYS = "days",
+        COOLDOWN_WEEKS = "weeks",
+        COOLDOWN_MONTHS = "months",
+        COOLDOWN_YEARS = "years"
       ].freeze
 
       included do
         attribute :cooldown_quantity, :integer, default: 2
-        enum :cooldown_type, COOLDOWN_TYPES.to_h { |t| [t, t] }, default: COOLDOWN_TYPE_SECONDS
+        enum :cooldown_type, COOLDOWN_TYPES.to_h { |t| [t, t] }, default: COOLDOWN_SECONDS
       end
 
       def cooldown_duration
-        return if cooldown_type == COOLDOWN_TYPE_NEVER
-        return cooldown_quantity.times if cooldown_type == COOLDOWN_TYPE_TIMES
+        return cooldown_quantity.times if cooldown_type == COOLDOWN_TIMES
 
         cooldown_quantity.public_send(cooldown_type)
       end
